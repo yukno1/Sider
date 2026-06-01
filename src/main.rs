6b50f1a97c5d54f79fe42b8781e16919e930812a
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-06-01 11:31:19 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
+                tokio::spawn(handle_conn(stream));
+            }
+            Err(e) => {
+                eprintln!("error: {}", e);
+                continue;
+            }
+        }
+    }
+}
+
+async fn handle_conn(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    loop {
+        match stream.read(&mut buffer).await {
+            Ok(size) if size != 0 => {
+                println!("Received: {:?}", buffer);
+                let response = "+PONG\r\n";
+
+                if let Err(e) = stream.write_all(response.as_bytes()).await {
+                    println!("Error writing to socket: {}", e);
+                }
+            }
+            Ok(_) => {
+                println!("Connection closed");
+                break;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+        }
+    }
+}
